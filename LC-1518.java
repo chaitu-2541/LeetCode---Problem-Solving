b13afc30f60563674bb9b50826fc56504f87a013
Chaitanya-Dev <76583086+chaitu-2541@users.noreply.github.com>
2025-10-21 19:16:00 +0530

    Water Bottles

---
+++ LC-1518.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int numWaterBottles(int numBottles, int numExchange) {
+        int drinked = 0;
+        int empBottles = 0;
+        while (numBottles > 0) {
+            drinked += numBottles;
+            empBottles += numBottles;
+
+            numBottles = empBottles/numExchange;
+            empBottles = empBottles % numExchange;
+        }
+        return drinked;
+    }
+} 
+    
+
