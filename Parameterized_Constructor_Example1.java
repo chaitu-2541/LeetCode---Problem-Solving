0dc56838398e62000ae1cc68d2606bef6a3030e5
Chaitanya-Dev <76583086+chaitu-2541@users.noreply.github.com>
2026-02-13 23:29:00 +0530

    Parametrized Constructor

---
+++ Parameterized_Constructor_Example1.java
@@ -0,0 +1,19 @@
+public class Parameterized_Constructor_Example1 {
+    int id;
+    String name; 
+    
+    Parameterized_Constructor_Example1(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    void display() {
+        System.out.println("ID: " + id);
+        System.out.println("Name: " + name);
+    }
+
+    public static void main(String[] args) {
+        Parameterized_Constructor_Example1 obj = new Parameterized_Constructor_Example1(101, "John");
+        obj.display();
+    }
+}
