e3345c94bcc918a249cdd175f95ae8e3c8b45824
Chaitanya-Dev <76583086+chaitu-2541@users.noreply.github.com>
2026-03-08 21:36:22 +0530

    Update LC2787.java

--- LC2787.java
+++ LC2787.java
@@ -15,7 +15,7 @@ class LC2787 {
     private static final int MOD = 1_000_000_007;
     public int numberOfWays(int n, int x) {
         long[] dp = new long[n + 1];
-        dp[0] = 1; 
+        dp[0] = 1;
         for (int i = 1; Math.pow(i, x) <= n; i++) {
             int power = (int) Math.pow(i, x);
             for (int sum = n; sum >= power; sum--) {
@@ -25,4 +25,5 @@ class LC2787 {
     }
 
 }
+
 
